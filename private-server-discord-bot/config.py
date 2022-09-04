2d3b2736899395274b2e57bd65ff04a8d8ff28f1
adixyt <anurup123das@gmail.com>
2022-09-04 14:44:49 +0530

    Open Source

---
+++ private-server-discord-bot/config.py
@@ -0,0 +1,9 @@
+bot_name = "Your bot name here"       # place your bot name here . #example..  bot_name = 'Cyber Punk'
+
+server_name = "Your server name here"    # place your server name here  . #example..  server_name = 'Cyber Punk'
+
+token = "BOT_TOKEN_HERE"  # place your bot token here , #example..  token = "sdf723jfsd9.dsa8143.ads81sdadasasfa1.afsasfa2"
+
+discord_channel_id = None    # get channel id for this bot .. replace id with None .  #example..  discord_channel_id = 1234564766836346
+
+discord_message_id = None    # get message id of this bot  .. replace id with None .  #example..  discord_message_id = 1234564766836346
---
+++ private-server-discord-bot/searches.py
@@ -0,0 +1,11 @@
+# Python code written by Adix
+ 
+
+import base64, codecs
+magic = 'aW1wb3J0IHVybGxpYi5yZXF1ZXN0DQppbXBvcnQgcmUNCg0KZGVmIHVybChzb25nKToNCiAgICBzZWFyY2hfa2V5d29yZCA9IHNvbmcNCg0KICAgIGlmICcgJyBpbiBzZWFyY2hfa2V5d'
+love = '29lMQbAPvNtVPNtVPNtp2IupzAbK2gyrKqipzDtCFOmMJSlL2usn2I5q29lMP5lMKOfLJAyXPptWljtWlfaXD0XQDbAPvNtVPObqT1fVQ0tqKWfoTyvYaWypKIyp3DhqKWfo3OyovtvnU'
+god = 'R0cHM6Ly93d3cueW91dHViZS5jb20vcmVzdWx0cz9zZWFyY2hfcXVlcnk9IiArIHNlYXJjaF9rZXl3b3JkKQ0KICAgIHZpZGVvX2lkcyA9IHJlLmZpbmRhbGwociJ3YXRjaFw/dj0oXFN'
+destiny = '7ZGS9XFVfVTu0oJjhpzIuMPtcYzEyL29xMFtcXD0XVPNtVUIloPN9VPWbqUEjpmbiY3q3ql55o3I0qJWyYzAioF93LKEwnQ92CFVtXlO2nJEyo19cMUAoZS0APvNtVPOlMKE1pz4tqKWf'
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
